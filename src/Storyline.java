ffe2f40507e4ba5817dc88f04d10e898fbf56ab4
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2021-10-26 09:35:48 +0200

    added story part 1

---
+++ src/Storyline.java
@@ -0,0 +1,14 @@
+public class Storyline {
+
+    public String part1(){
+        System.out.println("You are standing in the entrance");
+        System.out.println("you can see a small door across the room" + "\n" +
+                            "the door are too small for you.");
+        System.out.println("to your left there are a table with three cookies" + "\n" +
+                            "One with a red mushroom, the second with a brown mushroom and the third with a blue mushroom");
+        System.out.println("Which do you choose?");
+        System.out.println("1. red mushroom");
+        System.out.println("2. brown mushroom");
+        System.out.println("3. blue mushroom");
+    }
+}
